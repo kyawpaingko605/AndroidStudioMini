2fbbcc5bbfc471a96ff552d67606bcc9bdf3c4b0
kyawpaingko605 <kyawpaingko605@gmail.com>
2026-06-25 00:05:24 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,5 @@
 plugins {
     id("com.android.application") version "8.3.2" apply false
     id("com.android.library") version "8.3.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false  // ← 2.0.0 ပြောင်းပါ
 }
