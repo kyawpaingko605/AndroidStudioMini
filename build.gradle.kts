aa109c0a693bd74c94c62c5c5cc39113559f0af0
kyawpaingko605 <kyawpaingko605@gmail.com>
2026-06-25 00:45:56 +0700

    Upgrade Kotlin plugin to version 2.0.0
    Updated Kotlin plugin version from 1.9.25 to 2.0.0.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
     id("com.android.application") version "8.3.2" apply false
     id("com.android.library") version "8.3.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.25" apply false  // 1.9.22 မှ 1.9.25 ပြောင်းပါ
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
 }
